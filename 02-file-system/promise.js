33363eb0f92a6a162b862f52bf829f29ac7d2b77
aluno pos <alunopos@local.inatel.br>
2019-10-19 16:55:52 -0300

    Update promise.js example

--- 02-file-system/promise.js
+++ 02-file-system/promise.js
@@ -2,11 +2,18 @@
 function bakeACake() {
   console.log('Start baking the bake!');
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve('The cake is ready!'), 1000);
+    setTimeout(() => resolve('The cake is ready'), 1000);
     // reject('error');
   });
 }
 
+function addFrosting(cake) {
+  return new Promise((resolve, reject) => {
+    resolve(cake + ' with a delicious frosting!');
+  });
+}
+
 bakeACake()
+  .then(addFrosting)
   .then(data => console.log(data))
   .catch(error => console.log(error));
