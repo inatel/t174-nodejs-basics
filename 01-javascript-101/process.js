708fff02b135a7e67d31dd16ae2e512b8f34e3f0
aluno pos <alunopos@local.inatel.br>
2019-10-19 14:40:31 -0300

    Implement regular and fancy printer

--- 01-javascript-101/process.js
+++ 01-javascript-101/process.js
@@ -1,1 +1,14 @@
-console.log(process.argv);
+const names = process.argv.slice(2);
+
+function regularPrinter(name) {
+    console.log(name);
+}
+
+function fancyPrinter(name) {
+    console.log(`----> ${ name } <----`);
+}
+
+const print = fancyPrinter;
+names.forEach(print);
+
+print('Rodrigo')
