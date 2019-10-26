bc643a148aff8ed1423dbe0daa6600e617fb99b6
aluno pos <alunopos@local.inatel.br>
2019-10-26 14:26:18 -0300

    Add useful-string dependency

--- 04-npm/index.js
+++ 04-npm/index.js
@@ -1,3 +1,20 @@
 const chalk = require('chalk');
+const string = require('useful-string');
 
-console.log(chalk.yellow('Hi!'));
+const names = [
+  'Edson de Senna Junior',
+  'Filipe Henrique Benjamim de Arruda',
+  'Ário Guilherme Macedo',
+  'André Luiz Oneti Carvalho'
+]
+
+const byAlphabeticalOrder = (wordA, wordB) => 
+  ('' + wordA).localeCompare(wordB);
+
+const print = name => 
+  console.log(chalk.default.blue(name));
+
+names
+  .sort(byAlphabeticalOrder)
+  .map(string.underscore)
+  .forEach(print);
